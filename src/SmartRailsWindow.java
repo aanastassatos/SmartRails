1a8bfc1f6b0bfa02819b9624ea813c051f76b173
aanastassatos <aanastassatos@unm.edu>
2017-10-21 23:53:05 -0600

    TrackMaker now makes the track

---
+++ src/SmartRailsWindow.java
@@ -0,0 +1,26 @@
+import javafx.application.Application;
+import javafx.scene.Group;
+import javafx.scene.Scene;
+import javafx.scene.canvas.Canvas;
+import javafx.scene.layout.Pane;
+import javafx.stage.Stage;
+
+public class SmartRailsWindow extends Application
+{
+  
+  public static void main(String[] args)
+  {
+    launch(args);
+  }
+  
+  @Override
+  public void start(Stage stage) throws Exception
+  {
+    TrackMaker track = new TrackMaker();
+    Canvas canvas = new Canvas(1600, 800);
+    track.makeTrack(canvas.getGraphicsContext2D());
+    Scene scene = new Scene(new Group(canvas));
+    stage.setScene(scene);
+    stage.show();
+  }
+}
--- src/TrackMaker.java
+++ src/TrackMaker.java
@@ -1,5 +1,49 @@
+import javafx.scene.canvas.GraphicsContext;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+
 public class TrackMaker
 {
-  char [][] trackMap = {{'@', '-', '-', '-', '-', '@'},
-                        {'@', '-', '*', '%', '*', '@'}};
+  private final double IMAGE_SIZE = 200;
+  private final Image STRAIGHT_RAIL = res.ResourceLoader.getTrackImage("straightrail.png", 200, 200, false, false);
+  private final Image RED_LIGHT_RAIL = res.ResourceLoader.getTrackImage("redlightrail.png", 200, 200, false, false);
+  private final Image GREEN_LIGHT_RAIL = res.ResourceLoader.getTrackImage("greenlightrail.png", 200, 200, false, false);
+  private final Image STATION = res.ResourceLoader.getTrackImage("station.png", 200, 200, false, false);
+  char [][] trackMap = {{'-', '@', '%', '%', '%', '@', '*', '-'},
+                        {'@', '-', '-', '-', '-', '-', '-', '@'},
+                        {'@', '-', '*', '%', '*', '%', '-', '@'},
+                        {'@', '*', '-', '%', '-', '*', '-', '@'}};
+  
+  public void makeTrack(GraphicsContext gc)
+  {
+    for(int i = 0; i < trackMap.length; i++)
+    {
+      for (int j = 0; j < trackMap[i].length; j++)
+      {
+        char c = trackMap[i][j];
+        
+        switch (c)
+        {
+          case '@':
+            gc.drawImage(STATION, j*IMAGE_SIZE, i*IMAGE_SIZE);
+            break;
+          
+          case '-':
+            gc.drawImage(STRAIGHT_RAIL, j*IMAGE_SIZE, i*IMAGE_SIZE);
+            break;
+  
+          case '*':
+            gc.drawImage(GREEN_LIGHT_RAIL, j*IMAGE_SIZE, i*IMAGE_SIZE);
+            break;
+  
+          case '%':
+            gc.drawImage(RED_LIGHT_RAIL, j*IMAGE_SIZE, i*IMAGE_SIZE);
+            break;
+            
+          default:
+            break;
+        }
+      }
+    }
+  }
 }
